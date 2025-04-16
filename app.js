4836b9f136de6bd75e785d0eb61adf1b26ec636c
eltongaunagalera <eltongaunagalera@gmail.com>
2025-04-16 13:26:29 -0300

    Alteração do numero maximo para 50

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 // let paragrafo = document.querySelector('p');
 // paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 let listaNumeroEscolhido = [];
-let numeroMaximo = 100;
+let numeroMaximo = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
